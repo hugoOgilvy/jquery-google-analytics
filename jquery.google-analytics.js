995b59425445d90a47bbb85c890e536ffe3b6dd7
Joshua Jabbour <hey@joshuajabbour.com>
2009-04-20 15:01:45 -0700

    documentation cleanup

--- jquery.google-analytics.js
+++ jquery.google-analytics.js
@@ -1,78 +1,77 @@
 /*
 * jquery-google-analytics plugin
 *
-* A jQuery plugin that makes it easier to implement Google Analytics tracking, including event and link tracking.
+* A jQuery plugin that makes it easier to implement Google Analytics tracking,
+* including event and link tracking.
 *
 * Adds the following methods to jQuery:
-* - $.trackPage() - Adds Google Analytics tracking to the page it's called from.
-* - $('a').track() - Adds click tracking to elements.
-* - $.trackEvent() - Tracks an event using the given parameters. 
-* - $.timePageLoad() - Measures the time it takes  an event using the given parameters. 
+*   - $.trackPage() - Adds Google Analytics tracking on the page from which
+*     it's called.
+*   - $.trackEvent() - Tracks an event using the given parameters.
+*   - $('a').track() - Adds event tracking to element(s).
+*   - $.timePageLoad() - Measures the time it takes  an event using the given parameters.
 *
 * Features:
+*   - Improves page load time by loading Google Analytics code without blocking.
+*   - Easy and extensible event and link tracking plugin for jQuery and Google Analytics
+*   - Automatic internal/external link detection. Default behavior is to skip
+*     tracking of internal links.
+*   - Enforces that tracking event handler is added to an element only once.
+*   - Configurable: custom event tracking, skip internal links, metadata
+*     extraction using callbacks.
 *
-* - Improves page load times by loading Google Analytics code without blocking
-* - Easy and extensible event and link tracking plugin for jQuery and Google Analytics
-* - Automatic internal/external link detection. Default behavior is to skip tracking of internal links.
-* - Configurable: skip internal link tracking, metadata extraction using callbacks.
-* - Enforces that tracking event handler is added only once.
-*
-* Copyright (c) 2008 Christian Hellsten
+* Copyright (c) 2008-09 Christian Hellsten
 *
 * Plugin homepage:
-* http://aktagon.com/projects/jquery/google-analytics/
-* http://github.com/christianhellsten/jquery-google-analytics/
+*   http://aktagon.com/projects/jquery/google-analytics/
+*   http://github.com/christianhellsten/jquery-google-analytics/
 *
 * Examples:
-* http://aktagon.com/projects/jquery/google-analytics/examples/
-* http://code.google.com/apis/analytics/docs/eventTrackerGuide.html
+*   http://aktagon.com/projects/jquery/google-analytics/examples/
+*   http://code.google.com/apis/analytics/docs/eventTrackerGuide.html
 *
 * Repository:
-* git://github.com/christianhellsten/jquery-google-analytics.git
+*   git://github.com/christianhellsten/jquery-google-analytics.git
 *
 * Version 1.1.1
 *
 * Tested with:
-* - Mac: Firefox 3, Safari 3
-* - Linux: Firefox 3
-* - Windows: Firefox 3, Internet Explorer 6
+*   - Mac: Firefox 3, Safari 3
+*   - Linux: Firefox 3
+*   - Windows: Firefox 3, Internet Explorer 6
 *
 * Licensed under the MIT license:
 * http://www.opensource.org/licenses/mit-license.php
 *
 * Credits:
-*   - http://google.com/analytics: 
+*   - http://google.com/analytics
 *   - http://lyncd.com: 
 *       Idea for trackPage method came from this blog post: http://lyncd.com/2009/03/better-google-analytics-javascript/
-*
 */
-
 (function($) {
-
   var pageTracker;
 
   /**
-   * Enables Google Analytics tracking on the page it's called from. 
+   * Enables Google Analytics tracking on the page from which it's called.
    *
-   * Example:
-   *
-   *  <script type="text/javascript">
-   *    $.trackPage('UA-xxx-xxx', options);
-   *  </script>
+   * Usage:
+   *   <script type="text/javascript">
+   *     $.trackPage('UA-xxx-xxx', options);
+   *   </script>
    *
    * Parameters:
-   *
-   *  account_id - Your Google Analytics account ID.
-   *  options - An associative array containing one or more optional parameters:
-   *    - onload - If false, the Google Analytics code is loaded when this method is called instead of on window.onload.
-   *    - status_code - The HTTP status code of the current server response. If this is set to something other than 200 then the page is tracked as an error page. See this page for details: http://www.google.com/support/analytics/bin/answer.py?hl=en&answer=86927
-   *
+   *   account_id - Your Google Analytics account ID.
+   *   options - An object containing one or more optional parameters:
+   *     - onload - boolean - If false, the Google Analytics code is loaded
+   *       when this method is called instead of on window.onload.
+   *     - status_code - The HTTP status code of the current server response.
+   *       If this is set to something other than 200 then the page is tracked
+   *       as an error page. For more details: http://www.google.com/support/analytics/bin/answer.py?hl=en&answer=86927
    */
   $.trackPage = function(account_id, options) {
-    var host = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-    var script;
+    var host = ('https:' == document.location.protocol) ? 'https://ssl.' : 'http://www.';
 
-    // Use default options, if necessary
+    // Merge custom options with defaults.
     var settings = $.extend({}, {onload: true, status_code: 200}, options);
     var src = host + 'google-analytics.com/ga.js';
 
@@ -87,22 +86,21 @@
         }
         else {
           debug('Tracking error ' + settings.status_code);
-          pageTracker._trackPageview("/" + settings.status_code + ".html?page=" + document.location.pathname + document.location.search + "&from=" + document.referrer);
+          pageTracker._trackPageview('/' + settings.status_code + '.html?page=' + document.location.pathname + document.location.search + '&from=' + document.referrer);
         }
-
       }
       else { 
-        throw "_gat is undefined"; // setInterval loading?
+        throw '_gat is undefined'; // setInterval loading?
       }
     }
 
-    load_script = function() {
+    function load_script() {
       $.getScript(src, function() {
         init_analytics();
-      })
+      });
     }
 
-    // Enable tracking when called or on page load?
+    // Enable tracking when called or wait until page load?
     if (settings.onload == true || settings.onload == null) {
       $(window).load(load_script);
     }
@@ -114,11 +112,11 @@
   /**
    * Tracks an event using the given parameters.
    *
-   * The trackEvent method takes four arguments:
-   *   category - string, required - used to group events
-   *   action - string, required - used to name event
-   *   label - string, optional - used to provide more detail on event
-   *   value - integer, optional - used to aggregate numeric date about event
+   * Parameters:
+   *   category - string, required - Used to group events.
+   *   action - string, required - Used to name this event.
+   *   label - string, optional - Used to collect more details on this event.
+   *   value - integer, optional - Used to collect numeric data for this event.
    */
   $.trackEvent = function(category, action, label, value) {
     if (typeof pageTracker == 'undefined') {
@@ -130,8 +128,13 @@
   };
 
   /**
-   * Adds click tracking to element(s). Usage:
+   * Adds event tracking to element(s).
+   *
+   * Usage:
    *   $('a').track();
+   *
+   * Parameters:
+   *   An object containing one or more optional parameters.
    */
   $.fn.track = function(options) {
     // Add event handler to all matching elements.
